fb1e851e1b4a0850d1d6f6082775927c786adf19
Ibrahim Anjum <44471479+ianjum99@users.noreply.github.com>
2023-11-03 15:46:28 +0000

    Update outputs.tf

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,4 +1,14 @@
 output "bucket_id" {
-  value = aws_s3_bucket.app_source.id
+  description = "The ID (name) of the bucket."
+  value       = aws_s3_bucket.app_source.id
 }
 
+output "bucket_arn" {
+  description = "The ARN of the bucket."
+  value       = aws_s3_bucket.app_source.arn
+}
+
+output "bucket_region" {
+  description = "The region of the bucket."
+  value       = aws_s3_bucket.app_source.region
+}
