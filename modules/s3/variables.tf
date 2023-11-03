4768011551b33b84115a9dc94aadab82e00b2600
Ibrahim Anjum <44471479+ianjum99@users.noreply.github.com>
2023-11-03 15:43:18 +0000

    Update variables.tf

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -1,5 +1,15 @@
 variable "bucket_name" {
-  description = "The name of the bucket to create"
+  description = "The name of the S3 bucket where the application source will be stored."
   type        = string
 }
 
+variable "app_source_key" {
+  description = "The S3 key for the uploaded application zip file."
+  type        = string
+}
+
+variable "source_directory" {
+  description = "The local directory where the application zip file is located."
+  type        = string
+  default     = "../.."  # Adjust the path according to your project structure
+}
