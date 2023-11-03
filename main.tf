310aaf7e1d319b2ebe42900b69a62378412c0bbe
Ibrahim Anjum <44471479+ianjum99@users.noreply.github.com>
2023-11-03 14:34:26 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,19 +8,21 @@ module "s3_bucket" {
 }
 
 module "eb_application" {
-  source          = "./modules/eb_application"
-  application_name = "flask-weather-app"
-  description      = "Flask application for displaying weather data"
-  // Pass other required variables
+  source            = "./modules/eb_application"
+  application_name  = "flask-weather-app"
+  description       = "Flask application for displaying weather data"
+  solution_stack_name = "64bit Amazon Linux 2 v3.3.6 running Python 3.8"
+  instance_type      = "t2.micro"
+  min_size           = 2
+  max_size           = 4
+  vpc_id             = "vpc-12345678" # Replace with your actual VPC ID
 }
 
 module "security_group" {
   source  = "./modules/security_group"
-  vpc_id = "vpc-12345678" // Replace with your actual VPC ID
+  vpc_id = "vpc-12345678" # Replace with your actual VPC ID
 }
 
-# Use the outputs from your modules as needed
 output "application_url" {
   value = module.eb_application.eb_environment_hostname
 }
-
