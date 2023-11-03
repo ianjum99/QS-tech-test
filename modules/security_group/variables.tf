0dd3c36d86f246c1b334ab3ef37b4a4151ca1903
Ibrahim Anjum <44471479+ianjum99@users.noreply.github.com>
2023-11-03 15:49:10 +0000

    Update variables.tf

--- modules/security_group/variables.tf
+++ modules/security_group/variables.tf
@@ -3,3 +3,14 @@ variable "vpc_id" {
   type        = string
 }
 
+variable "security_group_name" {
+  description = "The name of the security group"
+  type        = string
+  default     = "eb-instances-sg"
+}
+
+variable "allow_http" {
+  description = "Indicates whether to allow HTTP traffic to the instances"
+  type        = bool
+  default     = true
+}
