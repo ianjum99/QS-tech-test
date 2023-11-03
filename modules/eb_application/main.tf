247aca79fbdd030ab4b3a1a774f72578405ff0b1
Ibrahim Anjum <44471479+ianjum99@users.noreply.github.com>
2023-11-03 15:40:31 +0000

    Update main.tf

--- modules/eb_application/main.tf
+++ modules/eb_application/main.tf
@@ -1,12 +1,24 @@
-ource "aws_elastic_beanstalk_application" "my_app" {
+resource "aws_elastic_beanstalk_application" "my_app" {
   name        = var.application_name
   description = var.description
+}
+
+resource "aws_elastic_beanstalk_application_version" "my_version" {
+  name        = "v1"
+  application = aws_elastic_beanstalk_application.my_app.name
+  description = "Version 1 of the app"
+
+  source_bundle {
+    s3_bucket = var.app_source_bucket
+    s3_key    = var.app_source_key
+  }
 }
 
 resource "aws_elastic_beanstalk_environment" "my_env" {
   name                = "${var.application_name}-env"
   application         = aws_elastic_beanstalk_application.my_app.name
   solution_stack_name = var.solution_stack_name
+  version_label       = aws_elastic_beanstalk_application_version.my_version.name
 
   setting {
     namespace = "aws:elasticbeanstalk:environment"
@@ -35,7 +47,14 @@ resource "aws_elastic_beanstalk_environment" "my_env" {
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "SecurityGroups"
-    value     = module.security_group.security_group_id
+    value     = var.sg_id
   }
 }
 
+output "eb_environment_hostname" {
+  value = aws_elastic_beanstalk_environment.my_env.cname
+}
+
+output "eb_environment_name" {
+  value = aws_elastic_beanstalk_environment.my_env.name
+}
