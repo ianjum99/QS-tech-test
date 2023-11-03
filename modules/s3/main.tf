ef252e93f4007df5f59f42cf634f02bd853829a8
Ibrahim Anjum <44471479+ianjum99@users.noreply.github.com>
2023-11-03 15:42:46 +0000

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,5 +1,15 @@
 resource "aws_s3_bucket" "app_source" {
   bucket = var.bucket_name
   acl    = "private"
+
+  versioning {
+    enabled = true
+  }
 }
 
+resource "aws_s3_bucket_object" "app_zip" {
+  bucket       = aws_s3_bucket.app_source.bucket
+  key          = var.app_source_key
+  source       = "${var.source_directory}/flask_app.zip"
+  etag         = filemd5("${var.source_directory}/flask_app.zip")
+}
