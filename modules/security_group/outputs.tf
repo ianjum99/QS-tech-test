4d9bff876f70f3fda313fd25a59422e4541ca265
Ibrahim Anjum <44471479+ianjum99@users.noreply.github.com>
2023-11-03 15:48:21 +0000

    Update outputs.tf

--- modules/security_group/outputs.tf
+++ modules/security_group/outputs.tf
@@ -1,4 +1,4 @@
 output "security_group_id" {
-  value = aws_security_group.eb_instances_sg.id
+  description = "The ID of the Elastic Beanstalk instance security group."
+  value       = aws_security_group.eb_instances_sg.id
 }
-
